22ec0ba0b808655be0b601f60cdfd76f9824f751
zmxn9941@naver.com <zmxn9941@naver.com>
2026-05-27 14:28:18 +0900

    feat:완료

---
+++ total_jpa_back/src/main/java/com/my/total_jpa_back/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.my.total_jpa_back.repository;
+
+import com.my.total_jpa_back.entity.Gender;
+import com.my.total_jpa_back.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+    // 1. 성별 조회
+    List<Users> findByGender(Gender gender);
+
+    // 2. 이름의 특정 문장을 포함하는 검색 :Containing (Like와 같다.)
+    List<Users> findByNameContaining(String keyword);
+
+    // 3. 좋아하는 색상 일치 자료 검색
+    List<Users> findByLikeColor(String color);
+
+    // 4. 색상과 성별로 검색하기
+    // SELECT * FROM users WHERE like_color ='red' AND gender = 'female'
+    List<Users> findByLikeColorAndGender(String color, Gender gender);
+}
